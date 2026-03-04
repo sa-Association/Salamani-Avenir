42f87a1dedf94d6e49729588e7bd5336ee2dd5af
Association <soilahanli1@gmail.com>
2026-03-04 11:08:39 +0300

    Update script.js
    modification fichier js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// MENU RESPONSIVE
 const toggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
-toggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
+if (toggle && navLinks) {
+    toggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
 
+// ANIMATION SCROLL
 const faders = document.querySelectorAll(".fade-in");
 
 const appearOnScroll = () => {
@@ -19,31 +23,57 @@ const appearOnScroll = () => {
 window.addEventListener("scroll", appearOnScroll);
 window.addEventListener("load", appearOnScroll);
 
-
 // MODE SOMBRE
 const darkToggle = document.getElementById("dark-mode-toggle");
 
-darkToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+if (darkToggle) {
+    darkToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
 
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("theme", "dark");
-        darkToggle.textContent = "☀️";
-    } else {
-        localStorage.setItem("theme", "light");
-        darkToggle.textContent = "🌙";
-    }
-});
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+            darkToggle.textContent = "☀️";
+        } else {
+            localStorage.setItem("theme", "light");
+            darkToggle.textContent = "🌙";
+        }
+    });
 
-// Sauvegarde du thème
-window.addEventListener("load", () => {
-    const savedTheme = localStorage.getItem("theme");
+    window.addEventListener("load", () => {
+        const savedTheme = localStorage.getItem("theme");
 
-    if (savedTheme === "dark") {
-        document.body.classList.add("dark-mode");
-        darkToggle.textContent = "☀️";
-    }
-});
+        if (savedTheme === "dark") {
+            document.body.classList.add("dark-mode");
+            darkToggle.textContent = "☀️";
+        }
+    });
+}
+
+// ACTUALITÉS
+const container = document.getElementById("liste-actualites");
+
+if (container) {
+    fetch('actualites.json')
+    .then(response => response.json())
+    .then(data => {
+
+        data.reverse().forEach(article => {
+
+            const card = document.createElement("div");
+            card.classList.add("carte");
+
+            card.innerHTML = `
+              <h2>${article.titre}</h2>
+              <small>${article.date}</small>
+              <p>${article.contenu}</p>
+            `;
+
+            container.appendChild(card);
+        });
+
+    });
+}
+
 
 
 
